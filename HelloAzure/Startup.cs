d442162ae1f3e40ee025f6c1fd89af48be140c02
Mateusz Zajac <TRBC37@motorolasolutions.com>
2020-02-04 10:28:15 +0100

    Revert "add homeControler, view and new properties in appsettings. delete pages catalog."
    This reverts commit 91bc8faa0cca37a488ab5285900c687c6f5bec92.

--- HelloAzure/Startup.cs
+++ HelloAzure/Startup.cs
@@ -64,12 +64,7 @@ namespace HelloAzure
 
             app.UseAuthentication();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
